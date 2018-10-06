f4e02f72df5e86f9ad5d6d67bd1394eb653d029c
moekofi <43388005+moekofi@users.noreply.github.com>
2018-10-06 12:16:03 -0400

    OpModeTwo
    This new Op mode is designed specifically for streamlining the strafing and normal driving functions onto the same Op mode by forcing only one to be active at a time (automatically of course). Also removed the compensation code for the backRight wheel.

--- liquidox/ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModeOne.java
+++ liquidox/ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModeOne.java
@@ -1,34 +1,34 @@
-package org.firstinspires.ftc.teamcode;
-
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
-
-
-@TeleOp
-public class OpModeOne extends OpMode {
-
-    DcMotor frontLeft, backLeft, frontRight, backRight;
-
-    public void init(){
-       //Naming the Motors for phone
-        frontLeft = hardwareMap.dcMotor.get("FL");
-        backLeft = hardwareMap.dcMotor.get("BL");
-        frontRight = hardwareMap.dcMotor.get("FR");
-        backRight = hardwareMap.dcMotor.get("BR");
-
-    }
-    public void loop(){
-       // forward and backward
-        frontLeft.setPower(-gamepad1.left_stick_y);
-        backLeft.setPower(-gamepad1.left_stick_y);
-        frontRight.setPower(gamepad1.left_stick_y);
-        backRight.setPower(2*gamepad1.left_stick_y);
-       // circular rotations (not strafing)
-        frontLeft.setPower(-gamepad1.right_stick_x);
-        backLeft.setPower(-gamepad1.right_stick_x);
-        frontRight.setPower(gamepad1.right_stick_x);
-        backRight.setPower(2*gamepad1.right_stick_x);
-
-    }
-}
+//package org.firstinspires.ftc.teamcode;
+//
+//import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+//import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+//import com.qualcomm.robotcore.hardware.DcMotor;
+//
+//
+//@TeleOp
+//public class OpModeOne extends OpMode {
+//
+//    DcMotor frontLeft, backLeft, frontRight, backRight;
+//
+//    public void init(){
+//       //Naming the Motors for phone
+//        frontLeft = hardwareMap.dcMotor.get("FL");
+//        backLeft = hardwareMap.dcMotor.get("BL");
+//        frontRight = hardwareMap.dcMotor.get("FR");
+//        backRight = hardwareMap.dcMotor.get("BR");
+//
+//    }
+//    public void loop(){
+//       // forward and backward
+//        frontLeft.setPower(-gamepad1.left_stick_y);
+//        backLeft.setPower(-gamepad1.left_stick_y);
+//        frontRight.setPower(gamepad1.left_stick_y);
+//        backRight.setPower(gamepad1.left_stick_y);
+//       // circular rotations (not strafing)
+//        frontLeft.setPower(-gamepad1.right_stick_x);
+//        backLeft.setPower(-gamepad1.right_stick_x);
+//        frontRight.setPower(gamepad1.right_stick_x);
+//        backRight.setPower(gamepad1.right_stick_x);
+//
+//    }
+//}
---
+++ liquidox/ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModeTwo.java
@@ -0,0 +1,42 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+@TeleOp
+public class OpModeTwo extends OpMode{
+
+    DcMotor frontLeft, backLeft, frontRight, backRight;
+
+    public void init(){
+        //Naming the Motors for phone
+        frontLeft = hardwareMap.dcMotor.get("FL");
+        backLeft = hardwareMap.dcMotor.get("BL");
+        frontRight = hardwareMap.dcMotor.get("FR");
+        backRight = hardwareMap.dcMotor.get("BR");
+
+    }
+    public void loop(){
+       if (gamepad1.left_stick_x > gamepad1.left_stick_y){
+           if (gamepad1.left_stick_x >= 0.5 || gamepad1.left_stick_x <= -0.5){
+               //insert strafing code
+               frontLeft.setPower(-gamepad1.left_stick_x);
+               backRight.setPower(gamepad1.left_stick_x);
+               frontRight.setPower(-gamepad1.left_stick_x);
+               backLeft.setPower(gamepad1.left_stick_x);
+           }
+       }else if (gamepad1.left_stick_y >= 0.5 || gamepad1.left_stick_y <= -0.5){
+           frontLeft.setPower(-gamepad1.left_stick_y);
+           backLeft.setPower(-gamepad1.left_stick_y);
+           frontRight.setPower(gamepad1.left_stick_y);
+           backRight.setPower(gamepad1.left_stick_y);
+       }else{
+           frontLeft.setPower(0);
+           backLeft.setPower(0);
+           frontRight.setPower(0);
+           backRight.setPower(0);
+       }
+
+    }
+}
+
