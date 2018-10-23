d858dda02e637c0b0b93cba7e5f874d5093c7ac3
moekofi <43388005+moekofi@users.noreply.github.com>
2018-10-23 19:57:50 -0400

    Carita is no longer the softwar manager
    I deleted the glitched OpMode withing OpMode BS that carita put into the code again

--- liquidox/ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModeTwo.java
+++ liquidox/ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModeTwo.java
@@ -2,34 +2,12 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-
 import java.util.concurrent.TimeUnit;
-
-//
-//
 //Hello! This entire section is just going to be a sort of logbook for all of the changes I intend to make
 //
 //Right now I am working on simplifying my cascading if statements into methods.
 //This will probably take a while but it will be worth it in the end.
-//Right now we are kind of waiting on hardware to add
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-
-
-
-
-
-
+//Right now we are kind of waiting on hardware to add something to work on but we should probably start on autonomous.
 @TeleOp
 public class OpModeTwo extends OpMode {
     DcMotor frontLeft, backLeft, frontRight, backRight;
@@ -41,7 +19,6 @@ public class OpModeTwo extends OpMode {
             return 2;
         }
     }
-
     public boolean boost (boolean a, boolean b, boolean x, boolean y){
     if (a == true && b == true && x == true && y == false){
     return true;
@@ -49,7 +26,12 @@ public class OpModeTwo extends OpMode {
         return false;
     }
     }
-
+    public void drive(double FL, double BL, double FR, double BR){
+        frontLeft.setPower(FL);
+        backRight.setPower(BR);
+        frontRight.setPower(FR);
+        backLeft.setPower(BL);
+    }
     public void init() {
         //Naming the Motors for phone
         frontLeft = hardwareMap.dcMotor.get("FL");
@@ -75,41 +57,17 @@ public class OpModeTwo extends OpMode {
         if (Math.abs(gamepad1.right_stick_x) > Math.abs(gamepad1.left_stick_y) && Math.abs(gamepad1.right_stick_x) > Math.abs(gamepad1.left_stick_x)) {
             if (gamepad1.left_stick_x >= 0.5 || gamepad1.left_stick_x <= -0.5) {
                 //insert strafing code
-                frontLeft.setPower(0.5  * sanic * gamepad1.right_stick_x);
-                backRight.setPower(-0.5 * sanic *gamepad1.right_stick_x);
-                frontRight.setPower(0.5 * sanic *gamepad1.right_stick_x);
-                backLeft.setPower(-0.5 * sanic *gamepad1.right_stick_x);
-                telemetry.addData("FL", gamepad1.right_stick_x);
-                telemetry.addData("BR", -gamepad1.right_stick_x);
-                telemetry.addData("FR", gamepad1.right_stick_x);
-                telemetry.addData("BL", -gamepad1.right_stick_x);
+                drive((0.5  * sanic * gamepad1.right_stick_x), (-0.5  * sanic * gamepad1.right_stick_x), (0.5  * sanic * gamepad1.right_stick_x), (-0.5  * sanic * gamepad1.right_stick_x));
             }
         } else if (Math.abs(gamepad1.left_stick_x) > Math.abs(gamepad1.left_stick_y)) {
             //turning
-            frontLeft.setPower(-0.5 * sanic *gamepad1.left_stick_x);
-            backLeft.setPower(-0.5 * sanic *gamepad1.left_stick_x);
-            frontRight.setPower(-0.5 * sanic *gamepad1.left_stick_x);
-            backRight.setPower(-0.5 * sanic *gamepad1.left_stick_x);
-            telemetry.addData("FL", -gamepad1.left_stick_x);
-            telemetry.addData("BL", -gamepad1.left_stick_x);
-            telemetry.addData("FR", -gamepad1.left_stick_x);
-            telemetry.addData("BR", -gamepad1.left_stick_x);
+            drive((-0.5  * sanic * gamepad1.right_stick_x), (-0.5  * sanic * gamepad1.right_stick_x), (-0.5  * sanic * gamepad1.right_stick_x), (-0.5  * sanic * gamepad1.right_stick_x));
         } else if (gamepad1.left_stick_y >= 0.5 || gamepad1.left_stick_y <= -0.5) {
             //basic front and back
-            frontLeft.setPower(-0.5 * sanic *gamepad1.left_stick_y);
-            backLeft.setPower(-0.5 * sanic *gamepad1.left_stick_y);
-            frontRight.setPower(0.5 * sanic *gamepad1.left_stick_y);
-            backRight.setPower(0.5 * sanic *gamepad1.left_stick_y);
-            telemetry.addData("FL", -gamepad1.left_stick_y);
-            telemetry.addData("BL", -gamepad1.left_stick_y);
+            drive((-0.5 * sanic *gamepad1.left_stick_y), (-0.5 * sanic *gamepad1.left_stick_y), (0.5 * sanic *gamepad1.left_stick_y), (0.5 * sanic *gamepad1.left_stick_y));
         }else{
             //it stops now
-            frontLeft.setPower(0);
-            backLeft.setPower(0);
-            frontRight.setPower(0);
-            backRight.setPower(0);
+            drive(0,0,0,0);
         }
-
-
     }
 }
