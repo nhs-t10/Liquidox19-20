32b9bc82e3033d40fcdc1fd159a2b08285d95760
Da Vinci Workshop <adamcarita@gmail.com>
2018-11-06 19:19:42 -0500

    op mode 6

---
+++ liquidox/ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModeSix.java
@@ -0,0 +1,68 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.Range;
+
+import java.util.concurrent.TimeUnit;
+
+@TeleOp
+public class OpModeSix extends OpMode {
+
+    DcMotor frontLeft, backLeft, frontRight, backRight;
+float sanic = 1;
+    public void init() {
+        /*Naming the Motors for phone*/
+        frontLeft = hardwareMap.dcMotor.get("FL");
+        backLeft = hardwareMap.dcMotor.get("BL");
+        frontRight = hardwareMap.dcMotor.get("FR");
+        backRight = hardwareMap.dcMotor.get("BR");
+
+    }
+
+    public void rightStickDrive(float front, float back) {
+
+        frontLeft.setPower(front);
+        backRight.setPower(-back);
+        frontRight.setPower(front);
+        backLeft.setPower(-back);
+
+    }
+
+    public void leftStickDrive(float right, float left){
+        frontLeft.setpower(left);
+        backRight.setPower(right);
+        frontRight.setPower(right);
+        backLeft.setPower(left);
+        }
+    public void loop() {
+
+        float lX = Range.clip(gamepad1.left_stick_x, -1, 1);
+        float lY = Range.clip(gamepad1.left_stick_y, -1, 1);
+        float rX = Range.clip(gamepad1.right_stick_x, -1, 1);
+
+
+        if (math.abs(lX)+math.abs(lY)>0.1) {
+
+
+            leftStickDrive(-(gamepad1.left_stick_x + gamepad1.left_stick_y) / 2 * sanic,
+                    -(gamepad1.left_stick_x - gamepad1.left_stick_y) / 2 * sanic;)
+
+        }else if(math.abs(rX)>0.1){
+
+            if (rX>0){
+               rightStickDrive(rX, rX);
+            }/*lse if(rX<0){
+                rightStickDrive( rX, rX)
+            } */
+
+
+
+        }
+    }
+
+}
+
+
+
