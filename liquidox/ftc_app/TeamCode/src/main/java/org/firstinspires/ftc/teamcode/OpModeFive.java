31e1025f95c63f1012903fe92466ecf1794cf0f7
shaashwat <shaash317@gmail.com>
2018-11-15 15:39:30 -0500

    Commented out servos tried to fic null point

--- liquidox/ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModeFive.java
+++ liquidox/ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModeFive.java
@@ -38,7 +38,7 @@ public class OpModeFive extends OpMode {
 
     }
 
-    float[] sum;
+    float[] sum = {0,0,0,0};
 
 
 
@@ -68,13 +68,13 @@ public class OpModeFive extends OpMode {
         drive(sum[0],sum[1],sum[2],sum[3]);
         //okay now that that masterpiece of coding is done, have some disgusting pasta.
         //if the button is down, move left and right shoulders forwards.
-            if(gamepad1.a) {
-              //  rightShoulder.setPosition(rightShoulder.getPosition()+1);
-                leftShoulder.setPosition(leftShoulder.getPosition()+1);
-            } /*no else because we don't want one button to "take precedence" over another-- might be jittery, but there you go `\_('-')_/` */ if (gamepad1.b) {
-            //rightShoulder.setPosition(rightShoulder.getPosition()-1);
-            leftShoulder.setPosition(leftShoulder.getPosition() - 1);
-        }
+//            if(gamepad1.a) {
+//              //  rightShoulder.setPosition(rightShoulder.getPosition()+1);
+//                leftShoulder.setPosition(leftShoulder.getPosition()+1);
+//            } /*no else because we don't want one button to "take precedence" over another-- might be jittery, but there you go `\_('-')_/` */ if (gamepad1.b) {
+//            //rightShoulder.setPosition(rightShoulder.getPosition()-1);
+//            leftShoulder.setPosition(leftShoulder.getPosition() - 1);
+//        }
         // why the heck did this show up here? }
 
     }
