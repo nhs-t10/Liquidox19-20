a266d408e10f848de9bad0049d6696956be9979c
shaashwat <shaash317@gmail.com>
2018-11-20 19:34:27 -0500

    Useful mistery thing and Autonomous thing

--- liquidox/ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousCrater.java
+++ liquidox/ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousCrater.java
@@ -1,4 +1,26 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import org.firstinspires.ftc.teamcode.Turning;
+
+@Autonomous
 public class AutonomousCrater extends AutonomousTesting {
+
+    public void loop() {
+
+        if (step == 1) {
+            unLatch();
+            if (timer1 >= 5000) {
+                step++;
+            }
+        }
+
+        if (step==2) {
+            //Turning.setDestination();
+        }
+
+    }
 }
+
+
