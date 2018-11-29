cb678b1bd7ba58f1a7bc001bb96119efab9705bb
ColeH <cbhreads2@gmail.com>
2018-11-28 19:42:51 -0500

    von...

--- liquidox/ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousCrater.java
+++ liquidox/ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousCrater.java
@@ -1,49 +1,49 @@
-package org.firstinspires.ftc.teamcode;
-
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import org.firstinspires.ftc.teamcode.Turning;
-import org.firstinspires.ftc.teamcode.BasicTankMode;
-
-@Autonomous
-public class AutonomousCrater extends AutonomousTesting {
-
-
-
-    /* WE STILL NEED TO INITIALIZE THE IMU, ASK PAUL WHAT WE HAVE OT DO FOR THAT */
-
-
-    public void loop() {
-
-        //init(); - not deleted because I don't want to mess up everything.
-
-        if (step == 1) {
-            unLatch();
-            if (timer1 >= 5000) {
-                step++;
-                drive(0, 0,0,0);
-            }
-        }
-
-        if (step == 2) {
-            drive(1,1,1,1);
-            if (timer1 >= 2000) {
-                step++;
-                drive(0, 0,0,0);
-            }
-
-        }
-
-        if (step==3) {
-            Turning.setDestination(90);
-            Turning.update();
-            if (Turning.getError < 3) {
-                step++;
-                drive(0, 0,0,0);
-            }
-        }
-
-    }
-}
-
-
+//package org.firstinspires.ftc.teamcode;
+//
+//import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+//import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+//import org.firstinspires.ftc.teamcode.Turning;
+//import org.firstinspires.ftc.teamcode.BasicTankMode;
+//
+//@Autonomous
+//public class AutonomousCrater extends AutonomousTesting {
+//
+//
+//
+//    /* WE STILL NEED TO INITIALIZE THE IMU, ASK PAUL WHAT WE HAVE OT DO FOR THAT */
+//
+//
+//    public void loop() {
+//
+//        //init(); - not deleted because I don't want to mess up everything.
+//
+//        if (step == 1) {
+//            unLatch();
+//            if (timer1 >= 5000) {
+//                step++;
+//                drive(0, 0,0,0);
+//            }
+//        }
+//
+//        if (step == 2) {
+//            drive(1,1,1,1);
+//            if (timer1 >= 2000) {
+//                step++;
+//                drive(0, 0,0,0);
+//            }
+//
+//        }
+//
+//        if (step==3) {
+//            Turning.setDestination(90);
+//            Turning.update();
+//            if (Turning.getError < 3) {
+//                step++;
+//                drive(0, 0,0,0);
+//            }
+//        }
+//
+//    }
+//}
+//
+//
