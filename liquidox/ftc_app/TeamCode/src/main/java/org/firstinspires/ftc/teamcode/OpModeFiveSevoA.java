ce0e43ad9d2b7ae6e7955f1a0b65c84952ea537c
moekofi <43388005+moekofi@users.noreply.github.com>
2018-11-30 16:41:47 -0500

    Started work on OMFSA

--- liquidox/ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModeFiveSevoA.java
+++ liquidox/ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModeFiveSevoA.java
@@ -7,7 +7,7 @@ import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.Range;
 
 @TeleOp
-public class OpModeFiveSevo extends OpMode {
+public class OpModeFiveSevoA extends OpMode {
     //private double random;
     //instantiate hardware devices
     DcMotor frontLeft, backLeft, frontRight, backRight;
@@ -31,11 +31,11 @@ public class OpModeFiveSevo extends OpMode {
     }
     public final void drive(float bl, float fl, float fr, float br ) {
 
-          frontLeft.setPower(-fl*9/5);
-          backRight.setPower(br*5/5);
-          frontRight.setPower(fr*5/5);
-          backLeft.setPower(-bl*9/5);
-
+        frontLeft.setPower(-fl*speed*9/10);
+        backRight.setPower(br*speed*4.5/10);
+        frontRight.setPower(fr*speed*4.5/10);
+        backLeft.setPower(-bl*speed*9/10);
+        
     }
 
     float[] sum = {0,0,0,0};
@@ -69,27 +69,31 @@ public class OpModeFiveSevo extends OpMode {
         drive(sum[0],sum[1],sum[2],sum[3]);
         //okay now that that masterpiece of coding is done, have some disgusting pasta.
         //if the button is down, move left and right shoulders forwards.
-            if(gamepad1.a) {
-                leftOuterShoulder.setPosition(leftOuterShoulder.getPosition()+1);
-                rightOuterShoulder.setPosition(rightOuterShoulder.getPosition()+1);
-            } /*no else because we don't want one button to "take precedence" over another-- might be jittery, but there you go `\_('-')_/` */
         if (gamepad1.b) {
-            leftChestShoulder.setPosition(leftOuterShoulder.getPosition()+1);
+            leftChestShoulder.setPosition(leftOuterShoulder.getPosition()-1);
             rightChestShoulder.setPosition(rightOuterShoulder.getPosition()+1);
         }
         if(gamepad1.a) {
-            leftOuterShoulder.setPosition(leftOuterShoulder.getPosition()-1);
+            leftOuterShoulder.setPosition(leftOuterShoulder.getPosition()+1);
             rightOuterShoulder.setPosition(rightOuterShoulder.getPosition()-1);
-        } /*no else because we don't want one button to "take precedence" over another-- might be jittery, but there you go `\_('-')_/` */
-
+        }
+        if (gamepad1.x) {
+            leftChestShoulder.setPosition(leftOuterShoulder.getPosition()+1);
+            rightChestShoulder.setPosition(rightOuterShoulder.getPosition()-1);
+        }
+        if(gamepad1.y) {
+            leftOuterShoulder.setPosition(leftOuterShoulder.getPosition()-1);
+            rightOuterShoulder.setPosition(rightOuterShoulder.getPosition()+1);
+        }
+        
         //if the left bumper is down, down the speed by 1.
         if(gamepad1.left_bumper) {
-            speed = 1/5f;
+            speed = 1.5f;
         } else if(gamepad1.right_bumper) {
-            speed = 4/5f;
+            speed = 4.5f;
         }
         else {
-            speed = 0.5f;
+            speed = 3f;
         }
         //////////////////////////////
 
