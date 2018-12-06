c6ce0c0f4829d9dd531b9651fdaab81fe5ecee3a
Adam <adamcarita@gmail.com>
2018-12-06 18:40:57 -0500

    Autonomous has been much better optimized (both)
    - using swich instead of if
    - fixed timer and other things in depot
    - values must be added back to both
    - ADDED AN EMERGANCY STOP (X)

--- liquidox/ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousCrater.java
+++ liquidox/ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousCrater.java
@@ -1,15 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
-import android.os.SystemClock;
-
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
-
-import org.firstinspires.ftc.teamcode.Turning;
-import org.firstinspires.ftc.teamcode.BasicTankMode;
-
-import java.text.SimpleDateFormat;
 import java.util.concurrent.TimeUnit;
 
 @Autonomous
@@ -20,8 +12,7 @@ public class AutonomousCrater extends LO2Library {
     /* WE STILL NEED TO INITIALIZE THE IMU, ASK PAUL WHAT WE HAVE OT DO FOR THAT */
     int step  = 1;
     ElapsedTime eTimeObj = new ElapsedTime();
-    boolean setTime = true;
-    float timer1, timerOffset;
+    float timer1;
     public void init(){
         /*Namiyng the Motors for phone*/
         frontLeft = hardwareMap.dcMotor.get("FL");
@@ -33,75 +24,124 @@ public class AutonomousCrater extends LO2Library {
 
     }
     public void loop() {
-
-
         timer1 = eTimeObj.time(TimeUnit.MILLISECONDS);
-        if (step == 1) {
-            /*We doon't have unlatch code*/
-//            unLatch();
-            if (timer1 >= 5000) {
-                LO2Library.drive(0f, 0f,0f,0f);
-                step++;
-            }
+        //emergency stop
+        if(gamepad1.x){
+            step = 8;
+        }
+        switch(step) {
+            case (1):
+                if (timer1 >= 5000) {
+                    drive(0f, 0f, 0f, 0f);
+                    step++;
+                }
+                break;
+
+            case (2):
+                if (timer1 >= 10000) {
+                    drive(0f, 0f, 0f, 0f);
+                    step++;
+                }
+                break;
+
+            case (3):
+                if (timer1 >= 15000) {
+                    drive(0f, 0f, 0f, 0f);
+                    step++;
+                }
+                break;
+            case (4):
+                if (timer1 >= 20000) {
+                    drive(0f, 0f, 0f, 0f);
+                    step++;
+                }
+                break;
+
+            case (5):
+                if (timer1 >= 25000) {
+                    drive(0f, 0f, 0f, 0f);
+                    step++;
+                }
+                break;
+
+            case (6):
+                if (timer1 >= 30000) {
+                    drive(0f, 0f, 0f, 0f);
+                    step++;
+                }
+                break;
+            default:
+                drive(0,0,0,0);
+                break;
         }
 
-
-
-        if (step == 2) {
-            LO2Library.drive(0.2f,0.2f,0.2f,0.2f);
-            if (timer1 >= 1500) {
-                LO2Library.drive(0f, 0f,0f,0f);
-                step++;
-            }
-
-        }
-
-        if (step == 3) {
-            Turning.setDestination(-90f);
-            Turning.update(Turning.currentAngle);
-            if (timer1 >= 6500) {
-                LO2Library.drive(0f, 0f,0f,0f);
-                step++;
-            }
-        }
-
-        if (step == 4) {
-            LO2Library.drive(0.2f,0.2f,0.2f, 0.2f);
-            if (timer1 >= 9500) {
-                LO2Library.drive(0f,0f,0f,0f);
-                step++;
-            }
-        }
-
-
-        if (step == 5) {
-            Turning.setDestination(-135f);
-            Turning.update(Turning.currentAngle);
-            if (timer1 >= 15500) {
-                LO2Library.drive(0f,0f,0f,0f);
-                step++;
-            }
-        }
-
-        if (step == 6) {
-            LO2Library.drive(0.2f,0.2f, 0.2f, 0.2f);
-            if (timer1 >= 14000) {
-                LO2Library.drive(0f,0f,0f,0f);
-                step++;
-            }
-        }
-
-        if (step == 7) {
-            LO2Library.drive(-1f,-1f,-1f,-1f);
-            if (timer1 == 16000) {
-                LO2Library.drive(0f,0f,0f,0f);
-            }
-        }
         telemetry.addData("Front Left Power: ", frontLeft.getPower());
         telemetry.addData("Front Right Power: ", frontRight.getPower());
         telemetry.addData("Back Left Power: ", backLeft.getPower());
         telemetry.addData("Back Right Power: ", backRight.getPower());
         telemetry.addData("Time: ", timer1 + "");
         telemetry.addData("Step: ", step + "");
+//        if (step == 1) {
+//            /*We don't have unlatch code*/
+////            unLatch();
+//            if (timer1 >= 5000) {
+//                LO2Library.drive(0f, 0f,0f,0f);
+//                step++;
+//            }
+//        }
+//
+//
+//
+//        if (step == 2) {
+//            LO2Library.drive(0.2f,0.2f,0.2f,0.2f);
+//            if (timer1 >= 1500) {
+//                LO2Library.drive(0f, 0f,0f,0f);
+//                step++;
+//            }
+//
+//        }
+//
+//        if (step == 3) {
+//            Turning.setDestination(-90f);
+//            Turning.update(Turning.currentAngle);
+//            if (timer1 >= 6500) {
+//                LO2Library.drive(0f, 0f,0f,0f);
+//                step++;
+//            }
+//        }
+//
+//        if (step == 4) {
+//            LO2Library.drive(0.2f,0.2f,0.2f, 0.2f);
+//            if (timer1 >= 9500) {
+//                LO2Library.drive(0f,0f,0f,0f);
+//                step++;
+//            }
+//        }
+//
+//
+//        if (step == 5) {
+//            Turning.setDestination(-135f);
+//            Turning.update(Turning.currentAngle);
+//            if (timer1 >= 15500) {
+//                LO2Library.drive(0f,0f,0f,0f);
+//                step++;
+//            }
+//        }
+//
+//        if (step == 6) {
+//            LO2Library.drive(0.2f,0.2f, 0.2f, 0.2f);
+//            if (timer1 >= 14000) {
+//                LO2Library.drive(0f,0f,0f,0f);
+//                step++;
+//            }
+//        }
+//
+//        if (step == 7) {
+//            LO2Library.drive(-1f,-1f,-1f,-1f);
+//            if (timer1 == 16000) {
+//                LO2Library.drive(0f,0f,0f,0f);
+//            }
+//        }
+
     }
 }
--- liquidox/ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousDepot.java
+++ liquidox/ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousDepot.java
@@ -3,58 +3,122 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
 import org.firstinspires.ftc.teamcode.Turning;
 import org.firstinspires.ftc.teamcode.BasicTankMode;
+
+import java.util.concurrent.TimeUnit;
 
 @Autonomous
 public class AutonomousDepot extends AutonomousTesting {
 
+    int step  = 1;
+    ElapsedTime eTimeObj = new ElapsedTime();
+    float timer1;
     public void init(){
-        init();
+        /*Namiyng the Motors for phone*/
+        frontLeft = hardwareMap.dcMotor.get("FL");
+        backLeft = hardwareMap.dcMotor.get("BL");
+        frontRight = hardwareMap.dcMotor.get("FR");
+        backRight = hardwareMap.dcMotor.get("BR");
+        step = 1;
     }
+    public void loop() {
+        timer1 = eTimeObj.time(TimeUnit.MILLISECONDS);
+        //emergency stop
+        if(gamepad1.x){
+            step = 30;
+        }
+        switch(step) {
+            case (1):
+                if (timer1 >= 5000) {
+                    drive(0f, 0f, 0f, 0f);
+                    step++;
+                }
+                break;
 
-    public void loop(){
+            case (2):
+                if (timer1 >= 10000) {
+                    drive(0f, 0f, 0f, 0f);
+                    step++;
+                }
+                break;
 
+            case (3):
+                if (timer1 >= 15000) {
+                    drive(0f, 0f, 0f, 0f);
+                    step++;
+                }
+                break;
+            case (4):
+                if (timer1 >= 20000) {
+                    drive(0f, 0f, 0f, 0f);
+                    step++;
+                }
+                break;
 
-        step = 2;
+            case (5):
+                if (timer1 >= 25000) {
+                    drive(0f, 0f, 0f, 0f);
+                    step++;
+                }
+                break;
 
-        if(step == 2){
-            LO2Library.drive(0.2f, 0.2f, 0.2f, 0.2f);
-            if(timer1 >= 1500){
-                LO2Library.drive(0f,0f,0f,0f);
-                timer1 = 0;
-                step++;
-            }
+            case (6):
+                if (timer1 >= 30000) {
+                    drive(0f, 0f, 0f, 0f);
+                    step++;
+                }
+                break;
+            default:
+                drive(0,0,0,0);
+                break;
         }
 
-        if(step == 3) {
-            Turning.setDestination(45);
-            Turning.update(Turning.currentAngle);
-
-        }
-
-            if (timer1 == 6500) {
-                LO2Library.drive(0f, 0f,0f,0f);
-                timer1 = 0;
-                step++;
-            }
-        }
-
-        if(step == 4) {
-            LO2Library.drive(-0.2f,0.2f,0.2f,-0.2f);
-            if(timer1 == 7000) {
-                LO2Library.drive(0f,0f,0f,0f);
-                timer1 = 0;
-                step++;
-            }
-        }
-
-        if(step == 5) {
-            LO2Library.drive(-1f,-1f,-1f,-1f);
-            if (timer1 == 6000) {
-                LO2Library.drive(0f,0f,0f,0f);
-            }
-        }
+        telemetry.addData("Front Left Power: ", frontLeft.getPower());
+        telemetry.addData("Front Right Power: ", frontRight.getPower());
+        telemetry.addData("Back Left Power: ", backLeft.getPower());
+        telemetry.addData("Back Right Power: ", backRight.getPower());
+        telemetry.addData("Time: ", timer1 + "");
+        telemetry.addData("Step: ", step + "");
+//        if(step == 2){
+//            LO2Library.drive(0.2f, 0.2f, 0.2f, 0.2f);
+//            if(timer1 >= 1500){
+//                LO2Library.drive(0f,0f,0f,0f);
+//                timer1 = 0;
+//                step++;
+//            }
+//        }
+//
+//        if(step == 3) {
+//            Turning.setDestination(45);
+//            Turning.update(Turning.currentAngle);
+//
+//        }
+//
+//            if (timer1 == 6500) {
+//                LO2Library.drive(0f, 0f,0f,0f);
+//                timer1 = 0;
+//                step++;
+//            }
+//        }
+//
+//        if(step == 4) {
+//            LO2Library.drive(-0.2f,0.2f,0.2f,-0.2f);
+//            if(timer1 == 7000) {
+//                LO2Library.drive(0f,0f,0f,0f);
+//                timer1 = 0;
+//                step++;
+//            }
+//        }
+//
+//        if(step == 5) {
+//            LO2Library.drive(-1f,-1f,-1f,-1f);
+//            if (timer1 == 6000) {
+//                LO2Library.drive(0f,0f,0f,0f);
+//            }
+//        }
 
     }
 }
