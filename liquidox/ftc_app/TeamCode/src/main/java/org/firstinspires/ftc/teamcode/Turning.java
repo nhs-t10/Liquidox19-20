8e69c0236e7c5562689960b84c3772416e9bf0a7
Adam <adamcarita@gmail.com>
2018-12-19 18:11:03 -0500

    Update Turning.java

--- liquidox/ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Turning.java
+++ liquidox/ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Turning.java
@@ -15,9 +15,9 @@ public class Turning {
     public static double currentAngle;
     private static double destination;
     private static double pComponent;
-    public static boolean turning = false;
+    private static boolean turning=false;
     private static final double P = 0.03;
-    public static boolean isDone;
+    public static double offSet;
 
     public static void Turning() {
         destination=0;
@@ -31,11 +31,12 @@ public class Turning {
     }
 
     public static void stopTurning(){
+        turning = false;
         LO2Library.drive(0f,0f,0f,0f);
     }
 
     public static void update(imuData imu) {
-        currentAngle = imu.getAngle();
+        currentAngle = imu.getAngle() - offSet;
         error = getError();
         pComponent = Range.clip(error * P,-1,1);
 
@@ -44,13 +45,17 @@ public class Turning {
             if (Math.abs(error) < 3) {
                 stopTurning();
             }
-                LO2Library.TurnDrive((pComponent * 0.16), (pComponent  * 0.16), -(pComponent  * 0.16), -(pComponent  * 0.16));
+            LO2Library.TurnDrive((pComponent * 0.16), (pComponent  * 0.16), -(pComponent  * 0.16), -(pComponent  * 0.16));
         }
 
     }
 
     public static double getError(){
         return currentAngle - destination ;
+    }
+
+    public static double get_angle(){
+        return currentAngle;
     }
 
 }
