226b8ae2952cde08f8bfc393a11dfc1260fa57aa
moekofi <43388005+moekofi@users.noreply.github.com>
2019-01-03 15:09:43 -0500

    Added in Latching code into OMVLatchesP

--- liquidox/ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LiftHandler.java
+++ liquidox/ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LiftHandler.java
@@ -1,24 +1,30 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class LiftHandler {
-    CRServo l;
+    CRServo latchS;
+    DcMotor latchM;
 
     public LiftHandler(HardwareMap h) {
-        this.l = h.crservo.get("Limb");
+        this.latchS = h.crservo.get("Limb");
     }
 
-    public void up() throws InterruptedException {
-        this.l.setPower(0.2);
+    public void upArm() throws InterruptedException {
+        this.latchS.setPower(0.2);
+        latchM.setPower(0.2f);
         Thread.sleep(1000);
-        this.l.setPower(0);
+        this.latchS.setPower(0);
+        latchM.setPower(0f);
     }
-    public void down() throws InterruptedException {
-        this.l.setPower(-0.2);
+    public void downArm() throws InterruptedException {
+        this.latchS.setPower(-0.2);
+        latchM.setPower(-0.2f);
         Thread.sleep(1000);
-        this.l.setPower(0);
+        this.latchS.setPower(0);
+        latchM.setPower(0f);
     }
 
 }
