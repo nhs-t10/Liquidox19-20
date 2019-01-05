a7606d2810ee5fe3d65e45dc151a32a9d80a67f6
ColeH <cbhreads2@gmail.com>
2019-01-05 12:39:45 -0500

    Fix for throttle problems

--- liquidox/ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModeFiveSevo.java
+++ liquidox/ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModeFiveSevo.java
@@ -24,7 +24,7 @@ public class OpModeFiveSevo extends OpMode {
         frontRight = hardwareMap.dcMotor.get("FR");
         backRight = hardwareMap.dcMotor.get("BR");
 
-       // assign shoulders (motors involved in arms)
+        // assign shoulders (motors involved in arms)
         rightChestShoulder = hardwareMap.servo.get("RCS");
         leftChestShoulder = hardwareMap.servo.get("LCS");
         rightOuterShoulder = hardwareMap.servo.get("ROS");
@@ -33,10 +33,10 @@ public class OpModeFiveSevo extends OpMode {
     }
     public final void drive(float bl, float fl, float fr, float br ) {
 /** Tells the robot how to drive */
-          frontLeft.setPower(-fl*speed); //Scaled by 0.8
-          backRight.setPower(br*speed);
-          frontRight.setPower(fr*speed);
-          backLeft.setPower(-bl*speed);
+        frontLeft.setPower(-fl*speed); //Scaled by 0.8
+        backRight.setPower(br*speed);
+        frontRight.setPower(fr*speed);
+        backLeft.setPower(-bl*speed);
 
     }
 
@@ -45,37 +45,39 @@ public class OpModeFiveSevo extends OpMode {
 
     public void loop() {
 /** finds the values from the controller*/
-        float lX = Range.clip(gamepad1.left_stick_x , -1, 1);
+        float lX = Range.clip(gamepad1.left_stick_x, -1, 1);
         float lY = Range.clip(gamepad1.left_stick_y, -1, 1);
-        float rX = Range.clip(gamepad1.right_stick_x/1.5f, -1, 1);
+        float rX = Range.clip(gamepad1.right_stick_x / 1.5f, -1, 1);
 
 /** creates driving modes */
         float[] vertical = {0.7f * lY, 0.7f * lY, 0.7f * lY, 0.7f * lY};
         float[] horizontal = {lX, -lX, lX, -lX};
         float[] rotational = {-0.7f * rX, -0.7f * rX, 0.7f * rX, 0.7f * rX};
 /** Adds all of the driving modes together */
-        for(int i=0; i<4; i++) {
+        for (int i = 0; i < 4; i++) {
             sum[i] = vertical[i] + horizontal[i] + rotational[i];
         }
 
         float highest = Math.max(Math.max(sum[0], sum[1]), Math.max(sum[2], sum[3]));
 /** Makes sure the robot doesnt drive above the maximum speed */
-        if(Math.abs(highest)>1) {
-            for (int i=0; i<4; i++) {
-                sum[i]=sum[i]/highest;
+        if (Math.abs(highest) > 1) {
+            for (int i = 0; i < 4; i++) {
+                sum[i] = sum[i] / highest;
             }
+
+
         }
 
-/* makes it go vroom*/
-        drive(sum[0],sum[1],sum[2],sum[3]);
+        /* makes it go vroom*/
+        drive(sum[0], sum[1], sum[2], sum[3]);
 
         //okay now that the pretty code is done, have some disgusting pasta.
         //if the button is down, move left and right shoulders forwards.
         /*moves outer servos if a button is pressed*/
-        if(gamepad1.a) {
-                leftOuterShoulder.setPosition(0.5);
-                rightOuterShoulder.setPosition(0.5);
-            } /*no else because we don't want one button to "take precedence" over another-- might be jittery, but there you go `\_('-')_/` */
+        if (gamepad1.a) {
+            leftOuterShoulder.setPosition(0.5);
+            rightOuterShoulder.setPosition(0.5);
+        } /*no else because we don't want one button to "take precedence" over another-- might be jittery, but there you go `\_('-')_/` */
         /*moves outer servos in opposite direction when b button is pressed*/
         if (gamepad1.b) {
             leftOuterShoulder.setPosition(0);
@@ -85,14 +87,17 @@ public class OpModeFiveSevo extends OpMode {
         //Throttle Code
 
         //If both bumpers are down, revert the speed to default
-        if(!gamepad1.left_bumper && !gamepad2.right_bumper) {
-           speed = 0.5;
-           //otherwise, if the left bumper is down, decrease the speed (with a minumum of 0)
-        } else if(gamepad1.left_bumper) {
-            speed = 0.3;
-        } else if(gamepad1.right_bumper) {
-            //then, if the right bumper is down, increase the speed (max of 5)
-            speed = 0.9;
+        if (!gamepad1.left_bumper && !gamepad2.right_bumper) {
+            speed = 0.5f;
+            //otherwise, if the left bumper is down, decrease the speed (with a minumum of 0)
+        } else {
+            if (gamepad1.left_bumper) {
+                speed = 0.3f;
+            }
+            if (gamepad1.right_bumper) {
+                //then, if the right bumper is down, increase the speed (max of 5)
+                speed = 0.9f;
+            }
         }
         //////////////////////////////
 
